262389b3452fea74b01d95648a237aff7b9097f5
nardsqq <nardparagas@gmail.com>
2019-04-07 19:14:29 +0800

    Set default imports within index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 const App = () => {
-  return <div>Hellow World!</div>
+  return <div className="bg-blue">Hello World!</div>
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));

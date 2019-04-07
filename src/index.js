1a7eba01f5fabd0416b2082e4e1f27d088bf8cbe
nardsqq <nardparagas@gmail.com>
2019-04-07 18:43:46 +0800

    Create a new base index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hellow World!</div>
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));

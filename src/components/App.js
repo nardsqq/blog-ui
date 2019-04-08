a66375587d3373301bde80c7bedb8f181a184285
nardsqq <nardparagas@gmail.com>
2019-04-08 16:51:26 +0800

    Fix input spacing in register form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Login from "./authentication/Login";
+import Register from "./authentication/Register";
 
 const App = () => {
     return (
         <div>
-            <Login />
+            <Register />
         </div>
     );
 };

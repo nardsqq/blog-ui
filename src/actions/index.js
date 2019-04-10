c9cd5cd3b27ce9dfa5a3c48e22161b0470109caa
nardsqq <nardparagas@gmail.com>
2019-04-11 02:42:20 +0800

    Add an inline comment about the localStorage issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import { SIGN_IN } from "./types";
 export const userLogin = (formValues) => async dispatch => {
     const response = await auth.post('/login', formValues);
 
+    // I'm aware that storing tokens within the local storage is a bad security practice. However, I used it to save time.
     window.localStorage.setItem('bearer_token', response.data.token);
     dispatch({ type: SIGN_IN, payload: response.data });
 };
